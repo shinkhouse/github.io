04f0dec954bbf640058b52406b89648a709c9c86
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2020-03-09 16:32:46 -0500

    mat-card header text style change add about me section

--- src/app/pages/home/components/projects/projects.component.ts
+++ src/app/pages/home/components/projects/projects.component.ts
@@ -14,25 +14,25 @@ export class ProjectsComponent implements OnInit {
         this.projects = [
             {
                 name: 'Test',
-                description: 'text description',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt lobortis neque in sodales. Nullam non gravida turpis, sit amet iaculis felis. Nulla id tempus risus. Curabitur porta non lorem sit amet egestas.',
                 url: 'www.google.com',
                 sourceCode: 'www.github.com'
             },
             {
                 name: 'Test',
-                description: 'text description',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt lobortis neque in sodales. Nullam non gravida turpis, sit amet iaculis felis. Nulla id tempus risus. Curabitur porta non lorem sit amet egestas.',
                 url: 'www.google.com',
                 sourceCode: 'www.github.com'
             },
             {
                 name: 'Test',
-                description: 'text description',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt lobortis neque in sodales. Nullam non gravida turpis, sit amet iaculis felis. Nulla id tempus risus. Curabitur porta non lorem sit amet egestas.',
                 url: 'www.google.com',
                 sourceCode: 'www.github.com'
             },
             {
                 name: 'Test',
-                description: 'text description',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt lobortis neque in sodales. Nullam non gravida turpis, sit amet iaculis felis. Nulla id tempus risus. Curabitur porta non lorem sit amet egestas.',
                 url: 'www.google.com',
                 sourceCode: 'www.github.com'
             },
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,15 +4,17 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PortfolioCardComponent } from 'src/app/shared/components/portfolio-card/portfolio-card.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule
   ],
-  declarations: [HomeComponent, ProjectsComponent],
+  declarations: [HomeComponent, ProjectsComponent, AboutMeComponent],
   exports: [
       HomeComponent,
+      AboutMeComponent,
       ProjectsComponent
   ]
 })
