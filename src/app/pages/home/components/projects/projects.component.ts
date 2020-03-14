b1eb6693195cb774072392df3af44c286b6b4d13
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2020-03-14 17:58:58 -0500

    add meta description, author, keywords add projects table add link styles add pwa icons add date to project model

--- src/app/pages/home/components/projects/projects.component.ts
+++ src/app/pages/home/components/projects/projects.component.ts
@@ -9,7 +9,9 @@ import { Project } from "src/app/shared/models/project";
 export class ProjectsComponent implements OnInit {
     constructor() {}
     public projects: Project[];
+    public showTable: boolean;
     ngOnInit(): void {
+        this.showTable = false;
         this.projects = [
             {
                 name: "Giphy Search",
@@ -17,7 +19,8 @@ export class ProjectsComponent implements OnInit {
                     "Angular based application to search for Gifs by keyword. Used Giphy API.",
                 url: "https://www.samuelhinkhouse.me/giphy",
                 sourceCode: "",
-                icon: "github"
+                icon: "github",
+                date: new Date("1/1/2019")
             },
             {
                 name: "Calculator",
@@ -25,29 +28,75 @@ export class ProjectsComponent implements OnInit {
                     "I made this calculator as one of my first projects with JavaScript.",
                 url: "https://www.samuelhinkhouse.me/JavaScript/calculator/",
                 sourceCode: "https://github.com/shinkhouse/calculator",
-                icon: "github"
+                icon: "github",
+                date: new Date("1/1/2017")
             },
             {
                 name: "Emoji Picker Menu",
                 description: "",
                 url: "https://codepen.io/shinkhouse/pen/wQpPYj",
                 sourceCode: "",
-                icon: "codepen"
+                icon: "codepen",
+                date: new Date("1/1/2019")
             },
             {
                 name: "Chat Application",
                 description: "",
                 url: "https://messaging-server-shinkhouse.herokuapp.com/",
                 sourceCode: "",
-                icon: "github"
+                icon: "github",
+                date: new Date("2/1/2019")
             },
             {
-                name: "Instagram Heart Effect",
-                description: "",
-                url: "https://codepen.io/shinkhouse/pen/KrjQoY",
-                sourceCode: "",
-                icon: "codepen"
+                name: "Portfolio v2",
+                description: "The second iteration of my portfolio website.",
+                sourceCode: "https://github.com/shinkhouse/portfolio-v2",
+                url: "https://www.samuelhinkhouse.me/v2",
+                icon: "github",
+                date: new Date("6/1/2016")
+            },
+            {
+                name: "Portfolio v3",
+                description: "The third iteration of my portfolio website.",
+                sourceCode: "https://github.com/shinkhouse/portfolio-v3",
+                url: "https://www.samuelhinkhouse.me/v3",
+                icon: "github",
+                date: new Date("9/1/2016")
+            },
+            {
+                name: "Portfolio v4",
+                description: "The fourth iteration of my portfolio website.",
+                sourceCode: "https://github.com/shinkhouse/portfolio-v4",
+                url: "https://www.samuelhinkhouse.me/v4",
+                icon: "github",
+                date: new Date("1/1/2018")
+            },
+            {
+                name: "Portfolio v6",
+                description: "The sixth iteration of my portfolio website.",
+                url: "https://www.samuelhinkhouse.me/v6",
+                sourceCode: "https://github.com/shinkhouse/portfolio-v6",
+                icon: "github",
+                date: new Date("6/1/2019")
+            },
+            {
+                name: "Felt Message Board",
+                description: "Inspired by my high school felt letter record board, I made this felt message generator.",
+                url: "https://www.samuelhinkhouse.me/projects/feltmessageboard",
+                sourceCode: "https://github.com/shinkhouse/felt-message-board",
+                icon: "github",
+                date: new Date("1/1/2017")
+            },
+            {
+                name: "Weather",
+                description: "Using Darksky and Google Maps apis, made this app to get the weather.",
+                url: "",
+                sourceCode: "https://github.com/shinkhouse/weather",
+                icon: "github",
+                date: new Date("2/1/2020")
             }
-        ];
+        ].sort(
+            (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+        );
     }
 }
--- src/app/shared/models/project.ts
+++ src/app/shared/models/project.ts
@@ -4,4 +4,5 @@ export interface Project {
     name: string;
     description: string;
     icon?: string;
+    date?: Date;
 }
