054848215430cf743d5ba6ea4c20ba480cbca583
Samuel Hinkhouse <hinkhouse.samuel@gmail.com>
2021-04-15 19:23:32 -0500

    update to angular 11

--- src/app/shared/components/portfolio-card/portfolio-card.component.spec.ts
+++ src/app/shared/components/portfolio-card/portfolio-card.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PortfolioCardComponent } from './portfolio-card.component';
 
@@ -6,7 +6,7 @@ describe('PortfolioCardComponent', () => {
     let component: PortfolioCardComponent;
     let fixture: ComponentFixture<PortfolioCardComponent>;
 
-    beforeEach(async(() => {
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [PortfolioCardComponent]
         }).compileComponents();
